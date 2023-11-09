cb37c6ef2b4ffff9f34d385f00315ba3ec16387d
Yassine Hanach <yassinedubraska@gmail.com>
2023-11-09 10:54:15 +0100

    first commit

---
+++ javas.js
@@ -0,0 +1,23 @@
+var hello = "Bienvenue à E-Learning ";
+ alert(hello)
+var jrs = 5;
+switch(jrs){
+    case 0 : alert("Aujourd'hui c'est dimanche");
+            break;
+    case 1 : alert("Aujourd'hui c'est lundi");
+            break;
+    case 2 : alert("Aujourd'hui c'est mardi");
+            break;
+    case 3 : alert("Aujourd'hui c'est mercredi");
+            break;
+    case 4 : alert("Aujourd'hui c'est jeudi");
+            break;
+    case 5 : alert("Aujourd'hui c'est vendredi");
+            break;
+    case 6 : alert("Aujourd'hui c'est samedi");
+            break;
+}
+
+var swab = "E-Learning vous éspère une bonne journée" ;
+alert(swab);
+
